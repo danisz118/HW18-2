2c06ab75117d48acee02d3fd28514aa8efa08bc1
danisz118 <daniz118@gmail.com>
2020-03-31 20:46:33 +0300

    initial commit

---
+++ src/main/java/Request.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+public class Request {
+    private Map<String, String> form;
+    private Map<String, String> headers;
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "form=" + form +
+                ", headers=" + headers +
+                '}';
+    }
+}
